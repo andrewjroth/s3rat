e1523c717e1c3f8f17a7a1c087ad81f1ca4c44bf
andrewjroth <andrew@andrewjroth.com>
2020-10-16 12:03:16 -0400

    Added interactive mode to client

--- s3rat/client.py
+++ s3rat/client.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import logging
+import sys
 from datetime import datetime
 
 from . import UTC, get_result_name, S3Comm
@@ -7,12 +8,15 @@ from . import UTC, get_result_name, S3Comm
 
 log = logging.getLogger(__name__ if __name__ != '__main__' else 'client')
 
+if sys.version_info.major == 2:
+    input = raw_input
+
 
 def cli_setup_parser(subparser):
     cli = subparser.add_parser('client', description="Send commands to S3 for server to execute")
     cli.add_argument('bucket', help="S3 Bucket name to use")
     cli.add_argument('session', help="Session ID")
-    group = cli.add_mutually_exclusive_group(required=True)
+    group = cli.add_mutually_exclusive_group()
     group.add_argument('--command', nargs='+', help="Single command to send")
     group.add_argument('--file', type=open, help="Script to send ('.py' or '.sh')")
     cli.set_defaults(func=cli_main)
@@ -25,18 +29,34 @@ def cli_main(args):
     print("Found Session ID:", sid)
     comm.wait_for("0_server_ready.txt")
     print("Server is ready")
-    timestamp = datetime.now(UTC).strftime("%H%M%SZ")
     if 'command' in args and args.command:
+        timestamp = datetime.now(UTC).strftime("%H%M%SZ")
         comm.upload("{}.cmd".format(timestamp), " ".join(args.command))
         print("Sent Command:", " ".join(args.command))
         result_name = get_result_name("{}.cmd".format(timestamp))
         comm.wait_for(result_name)
         print(comm.download(result_name))
-    elif 'file' in args:
+    elif 'file' in args and args.file:
+        timestamp = datetime.now(UTC).strftime("%H%M%SZ")
         comm.upload("{}_{}".format(timestamp, args.file.name), args.file.read())
         print("Sent Script:", "{}_{}".format(timestamp, args.file.name))
         result_name = get_result_name("{}_{}".format(timestamp, args.file.name))
         comm.wait_for(result_name)
         print(comm.download(result_name))
-    else:
-        pass  # TODO: Create interactive session
+    else:  # If no command and no file, start interactive command mode
+        print("Entering interactive mode... (type 'exit' to end)")
+        while True:
+            timestamp = datetime.now(UTC).strftime("%H%M%SZ")
+            try:
+                cmd = input('{} > '.format(timestamp))
+                if cmd == "exit":
+                    break
+                comm.upload("{}.cmd".format(timestamp), cmd)
+                result_name = get_result_name("{}.cmd".format(timestamp))
+                comm.wait_for(result_name)
+                print(comm.download(result_name))
+            except EOFError:
+                break
+            except KeyboardInterrupt:
+                break
+        print(" -- End -- ")
